3fe37ba80022a88699d3347db0ab77c8092e2f99
Mohammad Alom <malom1@fordham.edu>
2025-06-14 16:37:27 -0400

    handle flight search submission

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -3,11 +3,23 @@ import { useState } from "react"
 export default function Search() {
 
     const [flightNumber, setFlightNumber] = useState("")
+    const [flightData, setFlightData] = useState(null)
 
+    async function handleSubmit(e) {
+        e.preventDefault()
+        const res = await fetch(`http://localhost:3000/api/flight/${flightNumber}`)
+        if (res.ok) {
+            const data = await res.json()
+            setFlightData(data)
+        } else {
+            setFlightData(null)
+            alert("Flight not found")
+        }
+    }
     
     return(
         <div className="search">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Search for a flight</h2>
                 <h3>Flight Number: </h3>
                 <input 
@@ -17,6 +29,9 @@ export default function Search() {
                 />
                 <button type="submit">Submit</button>
             </form>
+            { flightData && (
+                <pre>{JSON.stringify(flightData, null, 2)}</pre>
+            )}
         </div>
     )
 }
