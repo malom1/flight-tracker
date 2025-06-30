4d15972e7b817671464c7376f103125ff2e3de66
Mohammad Alom <malom1@fordham.edu>
2025-06-30 15:55:26 -0400

    fix deployment config 2

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,10 @@ const app = express()
 const PORT = 4000
 
 app.use(cors())
-app.use(express.json()).use(express.static(path.join(__dirname, '/frontend/build')))
+app.use(express.static(path.join(__dirname, '/frontend/build')))
 
 app.get('*', (req, res) => 
-    res.sendFile(path.join(__dirname, '/client/build/index.html'))
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 )
 
 app.get('/api/flight/:ident', async (req, res) => {
